8e98e2c9665b889dd606dab870240fce58b1a638
Iryna Lipska <iranion.L@gmail.com>
2025-03-06 14:54:59 +0200

    Changed exercises with object

--- js/object.js
+++ js/object.js
@@ -6,54 +6,82 @@
 // Перевірка водія на наявність його ім’я у списку
 // Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину.
 
-let car = {
-  brand: "Ford",
-  model: "Kuga",
-  year: "2017",
-  speed: "90",
-  fuel: "60",
-  consumption: "6",
-  drivers: ["First", "Second"],
-};
+// const car = {
+//   brand: "Ford",
+//   model: "Kuga",
+//   year: 2017,
+//   speed: 90,
+//   fuel: 60,
+//   consumption: 6,
+//   drivers: ["First", "Second"],
 
-console.log(car);
+//   printCar() {
+//     Object.entries(this).forEach(([key, value]) => {
+//       if (typeof value !== "function") {
+//         console.log(`${key} : ${value}`);
+//       }
+//     });
+//   },
 
-car.drivers = [...car.drivers, "Third"];
+//   addDriver() {
+//     this.drivers = [...this.drivers, "Third"];
+//   },
 
-console.log(car.drivers);
+//   requaredTime(distance) {
+//     // let distance = +prompt("Enter your distance");
 
-let driverName = "Second";
+//     let time = distance / car.speed;
 
-if (car.drivers.includes(driverName)) {
-  console.log(`${driverName} is in list`);
-} else {
-  console.log(`${driverName} is not in list`);
-}
+//     if (time >= 4) {
+//       time = time + (time % 4);
+//     }
+//     let hours = 0;
+//     let minutes = 0;
+//     if (time < 1) {
+//       hours = 0;
+//       minutes = Math.round(time * 60);
+//     } else {
+//       hours = Math.round(time);
+//       minutes = Math.round((time - hours) * 60);
+//     }
+//     // alert(`Travel time is ${hours} hours ${minutes} minutes`);
+//     return { hours, minutes };
+//   },
+//   requaredFuel(distance) {
+//     let fuelQuantity = 0;
+//     fuelQuantity = (distance / 100) * 6;
+//     return fuelQuantity;
+//   },
+//   currentInfo() {
+//     let distance = +prompt("Enter your distance");
+//     const { hours, minutes } = this.requaredTime(distance);
+//     const fuelQuantity = this.requaredFuel(distance);
+//     alert(`Travel time is ${hours} hours ${minutes} minutes
+//   You need ${fuelQuantity} liters of fuel`);
+//   },
+// };
 
-let distance = +prompt("Enter your distance");
+// console.log(Object.entries(car));
+// car.printCar();
 
-function requaredTime(distance) {
-  let time = 0;
-  time = distance / car.speed;
-  if (time >= 4) {
-    time = time + (time % 4);
-  }
-  let hours = Math.round(time);
-  let minutes = Math.round((time - hours) * 60);
+// console.log(car.drivers);
 
-  return { hours, minutes };
-}
-let totalTime = requaredTime(distance);
+// car.addDriver();
 
-alert(`Travel time is ${totalTime.hours} hours ${totalTime.minutes} minutes`);
+// console.log(car.drivers);
+// car.drivers = [...car.drivers, "Third"];
 
-function requaredFuel(distance) {
-  let fuelQuantity = 0;
-  fuelQuantity = (distance / 100) * 6;
-  return fuelQuantity;
-}
+// console.log(car.drivers);
 
-alert(`You need ${requaredFuel(distance)} liters of fuel`);
+// let driverName = "Second";
+
+// if (car.drivers.includes(driverName)) {
+//   console.log(`${driverName} is in list`);
+// } else {
+//   console.log(`${driverName} is not in list`);
+// }
+
+// car.currentInfo();
 
 // Створити об'єкт, що описує час (години, хвилини, секунди), і такі функції для роботи з цим об'єктом:
 // Для виведення часу на екран.
@@ -65,39 +93,69 @@ alert(`You need ${requaredFuel(distance)} liters of fuel`);
 let clock = {
   hours: 9,
   minutes: 31,
-  second: 45,
+  seconds: 45,
+  formatTime(currentTime) {
+    currentTime = Number(currentTime);
+    return currentTime < 10 ? "0" + currentTime : currentTime;
+  },
+  printTime() {
+    const h = this.hours;
+    const m = this.minutes;
+    const s = this.seconds;
+
+    alert(
+      `Current time is ${this.formatTime(h)}:${this.formatTime(
+        m
+      )}:${this.formatTime(s)}`
+    );
+  },
+  addHour() {
+    let hours = +prompt("how many hours to add");
+    this.hours = (this.hours + hours) % 24;
+    this.printTime();
+  },
+  addMinutes() {
+    let minutes = +prompt("how many minutes to add");
+    if (this.minutes + minutes > 59) {
+      this.hours =
+        (this.hours + Math.trunc((this.minutes + minutes) / 60)) % 24;
+    }
+    this.minutes = (this.minutes + minutes) % 60;
+    this.printTime();
+  },
+  addSeconds() {
+    let seconds = +prompt("how many seconds to add");
+    console.log(this.seconds + seconds);
+    let minutes = 0;
+    if (this.seconds + seconds > 59) {
+      console.log(seconds);
+      minutes = this.minutes + Math.trunc((this.seconds + seconds) / 60);
+    }
+    console.log(minutes);
+    if (minutes > 59) {
+      this.hours =
+        (this.hours + Math.trunc((this.minutes + minutes) / 60)) % 24;
+    }
+    this.minutes = minutes % 60;
+    this.seconds = (this.seconds + seconds) % 60;
+    this.printTime();
+  },
 };
 
-function formatTime(currentTime) {
-  currentTime = Number(currentTime);
-  return currentTime < 10 ? "0" + currentTime : currentTime;
-}
+clock.printTime();
 
-let h = clock.hours;
-let m = clock.minutes;
-let s = clock.second;
+clock.addSeconds();
 
-console.log(
-  `Current time is ${formatTime(h)}:${formatTime(m)}:${formatTime(s)}`
-);
+// function updateClock() {
+//   const currentTimeIs = new Date();
+//   const blockCurrentTime = document.getElementById("clock");
+//   let currentHours = currentTimeIs.getHours().toString().padStart(2, "0");
+//   let currentMinutes = currentTimeIs.getMinutes().toString().padStart(2, "0");
+//   let currentSeconds = currentTimeIs.getSeconds().toString().padStart(2, "0");
+//   // console.log(`${currentHours}:${currentMinutes}:${currentSeconds}`);
+//   blockCurrentTime.textContent = `${currentHours}:${currentMinutes}:${currentSeconds}`;
+// }
 
-let h1 = +prompt("how many hours to add");
+// setInterval(updateClock(), 1000); //дія повторюється кожні 1000мсек=1сек
 
-function addHour(h1) {
-  clock.hours = (clock.hours + h1) % 24;
-  return clock.hours;
-}
-
-alert(`Now ${addHour(h1)} hours`);
-
-// console.log(student);
-// console.log(student.name);
-// student.name = 'Vlada';
-// console.log(student.name);
-// console.log(student);
-// console.log(student['age']);
-// console.log(student['skills']);
-
-// student.getFullName();
-
-// getFullName()
+// // setTimeout(updateClock(), 5000);  затримка виконання в мсек
